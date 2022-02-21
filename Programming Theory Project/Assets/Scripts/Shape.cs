cbcd923778001df453de1024d5a1afa22434b0b2
U-S-3-D <mguella@gmail.com>
2022-02-21 22:32:17 +0400

    Square Almost Done (Color Issue)
    Square's color's black instead of red for some reason.

---
+++ Programming Theory Project/Assets/Scripts/Shape.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    private int length;
+    private int width;
+
+    public int Length //ENCAPSULATION
+    {
+        get
+        {
+            return length;
+        }
+
+        set
+        {
+            if(value >= 1)
+            {
+                length = value;
+            }
+        }
+    }
+
+    public int Width //ENCAPSULATION
+    {
+        get
+        {
+            return width;
+        }
+
+        set
+        {
+            if(value >= 1)
+            {
+                width = value;
+            }
+        }
+    }
+    
+    public virtual void Area()
+    {
+        Debug.Log("The area is the amount of space taken up by a 2D shape.");
+    }
+
+    public virtual void ShapeColor()
+    {
+        //GetComponent<Renderer>().material.SetColor("_Color", Color.red); //Default color for a shape is red.
+    }
+}
---
+++ Programming Theory Project/Assets/Scripts/Square.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : Shape //INHERITANCE
+{
+    private int sqLength;
+    private int sqWidth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Area();
+        ShapeColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Area()
+    {
+        Square sq = new Square();
+        sq.Length = 1;
+        sq.Width = 2;
+        sqLength = sq.Length;
+        sqWidth = sq.Width;
+        Debug.Log("Area of Square is " + sq.Length * sq.Width);
+    }
+
+    public override void ShapeColor()
+    {
+        GameObject squareShape = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        squareShape.transform.localScale = new Vector2(sqWidth, sqLength);
+        //squareShape.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        squareShape.GetComponent<Renderer>().material.color = new Color(1,0,0);
+
+    }
+}
