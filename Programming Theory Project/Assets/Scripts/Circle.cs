8915ab8323847ad7c5ae3ece797e4e65e49b3cf0
U-S-3-D <mguella@gmail.com>
2022-02-23 21:53:51 +0400

    Circle Class Done
    Done.

--- Programming Theory Project/Assets/Scripts/Circle.cs
+++ Programming Theory Project/Assets/Scripts/Circle.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Circle : MonoBehaviour
+public class Circle : Shape //INHERITANCE
 {
     // Start is called before the first frame update
     void Start()
     {
+        Area(); //ABSTRACTION
+        ShapeColor(); //ABSTRACTION
+    }
+
+    public override void Area() //POLYMORPHISM
+    {
+        Length = 2;
+        Width = 2;
+        Debug.Log("Area of Circle is " + Length * Width * Mathf.PI);
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ShapeColor() //POLYMORPHISM
     {
-        
+        gameObject.transform.localScale = new Vector3(Width, Length, 1);
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
     }
 }
